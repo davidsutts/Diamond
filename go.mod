427433a977f32020331612ebd9194f6178b48c67
davidsutts <dsutton1202@gmail.com>
2024-05-22 09:33:31 +0930

    :sparkles: Basic Stripe Integration
    The index page has a basic payment option using the stripe quickstart guide.

--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module github.com/davidsutts/Diamond
 
 go 1.21.5
+
+require github.com/stripe/stripe-go v70.15.0+incompatible
+
+require (
+	github.com/stretchr/testify v1.7.0 // indirect
+	golang.org/x/net v0.0.0-20210520170846-37e1c6afe023 // indirect
+)
--- main.go
+++ main.go
@@ -1,16 +1,35 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"html/template"
+	"io"
+	"log"
 	"net/http"
+	"os"
+	"strings"
+
+	stripe "github.com/stripe/stripe-go"
+	"github.com/stripe/stripe-go/paymentintent"
 )
 
 var tmpl *template.Template
 
 func main() {
 
+	stripe.Key = os.Getenv("STRIPE_KEY")
+	if stripe.Key == "" {
+		log.Fatal("There is no stripe key")
+	}
+
 	// Create a webserver.
 	mux := http.NewServeMux()
+
+	// Redirect file requests to static dir.
+	mux.Handle("/static/", http.StripPrefix("/static", http.FileServer(http.Dir("static"))))
+
+	mux.HandleFunc("/create-payment-intent", paymentIntentHandler)
 	mux.HandleFunc("/", indexHandler)
 
 	http.ListenAndServe("127.0.0.1:8080", mux)
@@ -18,6 +37,63 @@ func main() {
 }
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println(r.URL)
 	tmpl = template.Must(template.ParseFiles("static/html/index.html"))
 	tmpl.Execute(w, nil)
+}
+
+func paymentIntentHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println(r.URL)
+	if r.Method != http.MethodPost {
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+		return
+	}
+
+	// Parse the incoming request.
+	err := r.ParseForm()
+	if err != nil {
+		log.Println("failed to parse form:", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	products := strings.Split(r.FormValue("products"), ",")
+
+	params := stripe.PaymentIntentParams{
+		Amount:   stripe.Int64(calcAmount(products)),
+		Currency: stripe.String(string(stripe.CurrencyAUD)),
+	}
+
+	pi, err := paymentintent.New(&params)
+	if err != nil {
+		log.Println("failed to create new payment intent:", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	log.Println("New payment intent created:", pi.ClientSecret)
+
+	// Write client secret to response writer.
+	writeJSON(w, struct {
+		ClientSecret string `json:"clientSecret"`
+	}{
+		ClientSecret: pi.ClientSecret,
+	})
+}
+
+func writeJSON(w http.ResponseWriter, v interface{}) {
+	var buf bytes.Buffer
+	if err := json.NewEncoder(&buf).Encode(v); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		log.Printf("json.NewEncoder.Encode: %v", err)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	if _, err := io.Copy(w, &buf); err != nil {
+		log.Printf("io.Copy: %v", err)
+		return
+	}
+}
+
+func calcAmount(product []string) int64 {
+	return 1500
 }
